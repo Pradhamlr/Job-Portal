1da5e2a21a422bfd3ef8d993370e13ba7b500ac1
Pradhamlr <pradham.lr@gmail.com>
2025-07-09 19:31:29 +0530

    First Commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react'
+import { assets } from '../assets/assets'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+import { Link, useNavigate } from 'react-router-dom';
+import { AppContext } from '../context/AppContext';
+
+const Navbar = () => {
+
+    const { openSignIn } = useClerk()
+    const { user } = useUser()
+
+    const navigate = useNavigate()
+
+    const {setShowRecruiterLogin} = useContext(AppContext)
+
+  return (
+    <header className='shadow py-4'>
+        <div className='container px-4 2xl:px-20 flex justify-between items-center mx-auto'>
+            <img onClick={() => navigate('/')} className='cursor-pointer' src={assets.logo} alt="Logo" /> 
+            {
+                user
+                ?<div className='flex items-center gap-3'>
+                    <Link to={'/applications'}>Applied Jobs</Link>
+                    <p>|</p>
+                    <p className='max-sm:hidden'>Hi, {`${user.firstName}`}</p>
+                    <UserButton />
+                </div>
+                :<div className='flex gap-6 max-sm:text-xs'>
+                <button onClick={e => setShowRecruiterLogin(true)} className='cursor-pointer text-[#545454]'>Recruiter Login</button>
+                <button onClick={openSignIn} className='bg-[#0260FF] text-[#FFFFFF] rounded-full px-6 sm:px-9 py-2 cursor-pointer'>Login</button>
+            </div>
+            }
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ client/src/pages/ManageJobs.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { manageJobsData } from '../assets/assets'
+import moment from 'moment'
+import { useNavigate } from 'react-router-dom'
+
+const ManageJobs = () => {
+
+        const navigate = useNavigate()
+
+  return (
+    <div className='container p-4 max-w-5xl'>
+        <div className='overflow-x-auto'>
+            <table className='min-w-full bg-qhite border border-gray-200 max-sm:text-sm'>
+                <thead>
+                    <tr>
+                        <th className='max-sm:hidden py-2 px-4 border-b border-gray-200 text-left'>#</th>
+                        <th className='py-2 px-4 border-b border-gray-200 text-left'>Job Title</th>
+                        <th className='py-2 px-4 border-b border-gray-200 text-left max-sm:hidden'>Date</th>
+                        <th className='py-2 px-4 border-b border-gray-200 text-left max-sm:hidden'>Location</th>
+                        <th className='py-2 px-4 border-b border-gray-200 text-center'>Applicants</th>
+                        <th className='py-2 px-4 border-b border-gray-200 text-left'>Visible</th>
+                    </tr>
+                </thead>
+                <tbody>
+                   {manageJobsData.map((job, index) => (
+                    <tr key={index} className='text-gray-700'>
+                        <td className='max-sm:hidden py-2 px-4 border-b border-gray-200'>{index + 1}</td>
+                        <td className='py-2 px-4 border-b border-gray-200'>{job.title}</td>
+                        <td className='max-sm:hidden py-2 px-4 border-b border-gray-200'>{moment(job.date).format('ll')}</td>
+                        <td className='max-sm:hidden py-2 px-4 border-b border-gray-200'>{job.location}</td>
+                        <td className='py-2 px-4 border-b border-gray-200 text-center'>{job.applicants}</td>
+                        <td className='py-2 px-4 border-b border-gray-200'>
+                            <input className='scalw-125 ml-4' type="checkbox"/>
+                        </td>
+                    </tr>
+                   ))} 
+                </tbody>
+            </table>
+        </div>
+        <div className='mt-4 flex justify-end'>
+            <button onClick={() => navigate('/dashboard/add-job')} className='cursor-pointer bg-black text-white py-2 px-4 rounded'>Add New Job</button>
+        </div>
+    </div>
+  )
+}
+
+export default ManageJobs
