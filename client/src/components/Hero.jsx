1be0d3c693932a8334faa4b7c9d89896bdf6eb0e
Pradhamlr <pradham.lr@gmail.com>
2025-07-19 22:46:22 +0530

    Trusted Section Hidden

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
             </div>
         </div>
 
-        <div className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex '>
+        {/* <div className='border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex '>
             <div className='flex justify-center gap-10 lg:gap-16 flex-wrap'>
                 <h2 className='text-[#7E7396] font-medium'>Trusted By</h2>
                 <img className='h-6' src={assets.microsoft_logo} alt="Microsoft" />
@@ -51,7 +51,7 @@ const Hero = () => {
                 <img className='h-6' src={assets.amazon_logo} alt="Amazon" />
                 <img className='h-6' src={assets.adobe_logo} alt="Adobe" />
             </div>
-        </div>
+        </div> */}
     </div>
   )
 }
